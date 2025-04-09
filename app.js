5368cd55199ce0fca712c0ee55d4339b999927ba
Jatin <jatin333x@gmail.com>
2025-04-09 14:39:30 +0530

    initial commit 2

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-import express from 'express';
+const express = require('express');
+const app = express();
+
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+app.use(express.json());  // Parse JSON data
+app.use(express.urlencoded({ extended: true }));  // Parse URL-encoded data
+app.use(cookieParser());  // Parse cookies
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the public directory
+app.use("view engine", "ejs"); // Set EJS as the view engine 
+
+app.get('/', (req, res) => {});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
