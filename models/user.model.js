cab3f38512cd5483a973a2ea282bc65089affd98
Jatin <jatin333x@gmail.com>
2025-04-09 14:43:04 +0530

    initial upt 2

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/sctach');
+mongoose.connect('mongodb://127.0.0.1:27017/scatch');
 
 const userSchema = mongoose.Schema({
     fullname:String,
